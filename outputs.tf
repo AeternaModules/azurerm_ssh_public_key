1fe037ff7201ee94ec7894cf4c097fb681b19fa3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "ssh_public_keys" {
-  description = "All ssh_public_key resources"
-  value       = azurerm_ssh_public_key.ssh_public_keys
-}
 output "ssh_public_keys_location" {
-  description = "List of location values across all ssh_public_keys"
-  value       = [for k, v in azurerm_ssh_public_key.ssh_public_keys : v.location]
+  description = "Map of location values across all ssh_public_keys, keyed the same as var.ssh_public_keys"
+  value       = { for k, v in azurerm_ssh_public_key.ssh_public_keys : k => v.location }
 }
 output "ssh_public_keys_name" {
-  description = "List of name values across all ssh_public_keys"
-  value       = [for k, v in azurerm_ssh_public_key.ssh_public_keys : v.name]
+  description = "Map of name values across all ssh_public_keys, keyed the same as var.ssh_public_keys"
+  value       = { for k, v in azurerm_ssh_public_key.ssh_public_keys : k => v.name }
 }
 output "ssh_public_keys_public_key" {
-  description = "List of public_key values across all ssh_public_keys"
-  value       = [for k, v in azurerm_ssh_public_key.ssh_public_keys : v.public_key]
+  description = "Map of public_key values across all ssh_public_keys, keyed the same as var.ssh_public_keys"
+  value       = { for k, v in azurerm_ssh_public_key.ssh_public_keys : k => v.public_key }
 }
 output "ssh_public_keys_resource_group_name" {
-  description = "List of resource_group_name values across all ssh_public_keys"
-  value       = [for k, v in azurerm_ssh_public_key.ssh_public_keys : v.resource_group_name]
+  description = "Map of resource_group_name values across all ssh_public_keys, keyed the same as var.ssh_public_keys"
+  value       = { for k, v in azurerm_ssh_public_key.ssh_public_keys : k => v.resource_group_name }
 }
 output "ssh_public_keys_tags" {
-  description = "List of tags values across all ssh_public_keys"
-  value       = [for k, v in azurerm_ssh_public_key.ssh_public_keys : v.tags]
+  description = "Map of tags values across all ssh_public_keys, keyed the same as var.ssh_public_keys"
+  value       = { for k, v in azurerm_ssh_public_key.ssh_public_keys : k => v.tags }
 }
 
